bcde6a82d59f9287041d898bee8ed315001cded1
Jack Bird <jack.bird@durham.ac.uk>
2021-08-29 17:30:40 +0100

    Added momentary config option

--- config.def.h
+++ config.def.h
@@ -29,6 +29,8 @@ static const char *colors[][3]      = {
 
 /* tagging */
 static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+static const char *tagsalt[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+static const int momentaryalttags = 0; /* 1 means alttags will show only when key is held down*/
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -111,6 +113,7 @@ static Key keys[] = {
 	TAGKEYS(                        XK_8,                      7)
 	TAGKEYS(                        XK_9,                      8)
 	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },	
+	{ MODKEY,                       XK_n,      togglealttag,   {0} },
 	{ MODKEY,                       XK_c,      spawn,          SHCMD("google-chrome-stable --proxy-server=http://127.0.0.1:8889") },
 	{ MODKEY,                       XK_e,      spawn,          SHCMD("export https_proxy=http://127.0.0.1:8889;emacs") },
 };
